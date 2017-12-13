4d8ba2761aa0da77c967bf7ee4204b7a36a9a51b
taishikato <taishi.k0903@gmail.com>
2017-12-14 00:12:19 +0900

    20171213

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCygaLebfIQay8vjiErlBCz_EmCkhWz1co",
+  authDomain: "guidesquare-test.firebaseapp.com",
+  databaseURL: "https://guidesquare-test.firebaseio.com",
+  projectId: "guidesquare-test",
+  storageBucket: "guidesquare-test.appspot.com",
+  messagingSenderId: "793631402353"
+}
